20e9e6e32e0fa8f399418333f12e5cbf4a00d35e
ishan111 <ishankaizen@gmail.com>
2017-11-27 22:51:11 +0530

    Cloned to git TRIAL
    HI

--- aakashMod1/read.cpp
+++ aakashMod1/read.cpp
@@ -2,28 +2,28 @@
 
 void read(){
     int nparams = 14;
-   /* string comment[nparams];
+    string comment[nparams];
     string input[nparams];
 
     for(int i = 0; i < nparams; i++){
         getline(cin, comment[i]);
         getline(cin, input[i]);
-    }*/
+    }
 
-    cin>>potential_type;
-    cin>>sigma;
-    cin>>bfactor;
-    cin>>ncells ;
-    cin>>periodic;
-    cin>>ncycles;
-    cin>>samp_ival;
-    cin>>zz;
-    cin>>exc_disp_ratio;
-    cin>>delta;
-    cin>>rmax_factor;
-    cin>>dr_factor;
-    cin>>logging;
-    cin>>read_from_file;
+    potential_type = stoi(input[0]); 
+    sigma = stod(input[1]);
+    bfactor = stod(input[2]);
+    ncells = stoi(input[3]);
+    periodic = (bool) stod(input[4]);
+    ncycles = stoi(input[5]);
+    samp_ival = stoi(input[6]);
+    zz = stod(input[7]);
+    exc_disp_ratio = stod(input[8]);
+    delta = stod(input[9]);
+    rmax_factor = stoi(input[10]);
+    dr_factor = stoi(input[11]);
+    logging = stoi(input[12]);
+    read_from_file = (bool) stoi(input[13]);
 
     L = bfactor * sigma; 
 }
